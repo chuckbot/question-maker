0100060e2475560cd45b1714240d83960f90358d
Carlos <cgmilan@gmail.com>
2021-04-19 02:23:56 -0500

    Styles

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script>
   import { db } from "./firebase";
+  import toastr from "toastr";
 
   let quizForm = [
     { type: "textarea", value: "", name: "question" },
@@ -38,7 +39,11 @@
         ...finalAnswers,
         createdAt: Date.now(),
       });
-    console.log("New question created");
+    toastr.success("New question created", {
+      timeOut: 1000,
+      progressBar: true,
+      positionClass: "toast-bottom-right",
+    });
   };
 
   const handleAnswer = (name) => {
@@ -51,33 +56,49 @@
   };
 </script>
 
-<form on:submit|preventDefault={handleSubmit}>
-  {#each quizForm as field, index}
-    {#if field.type === "input"}
-      <input
-        on:click={() => handleAnswer(field.name)}
-        bind:value={field.value}
-        type="text"
-        placeholder=""
-      />
-      {#if stateArr[options.findIndex((x) => x.name === field.name)]}
-        <p>Respuesta Correcta</p>
-      {/if}
-    {:else if field.type === "textarea"}
-      <textarea
-        bind:value={field.value}
-        rows="3"
-        placeholder="Escribe una pregunta"
-      />
-    {/if}
-  {/each}
-  <button type="submit">Guardar</button>
-</form>
+<div class="container p-4">
+  <div class="row">
+    <div class="col-md-6 offset-md-3">
+      <form on:submit|preventDefault={handleSubmit} class="card card-body">
+        {#each quizForm as field, index}
+          {#if field.type === "input"}
+            <div class="form-group">
+              <input
+                on:click={() => handleAnswer(field.name)}
+                bind:value={field.value}
+                class="form-control"
+                type="text"
+                placeholder="Inserta una opcion"
+              />
+              {#if stateArr[options.findIndex((x) => x.name === field.name)]}
+                <p>Respuesta Correcta</p>
+              {/if}
+            </div>
+          {:else if field.type === "textarea"}
+            <div class="form-group">
+              <textarea
+                bind:value={field.value}
+                class="form-control"
+                rows="3"
+                placeholder="Escribe una pregunta"
+              />
+            </div>
+          {/if}
+        {/each}
+        <button class="btn btn-primary">Guardar</button>
+      </form>
 
-{#each quizForms as quizForm}
-  <h5>{quizForm.question}</h5>
-  <button on:click={deleteQuiz(quizForm.id)}>Eliminar</button>
-{/each}
+      {#each quizForms as quizForm}
+        <div class="card card-body mt-2">
+          <h5>{quizForm.question}</h5>
+          <button class="btn btn-danger" on:click={deleteQuiz(quizForm.id)}
+            >Eliminar</button
+          >
+        </div>
+      {/each}
+    </div>
+  </div>
+</div>
 
 <style>
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import App from "./App.svelte";
 import "./firebase";
+import "bootswatch/dist/darkly/bootstrap.min.css";
+import "toastr/build/toastr.min.js";
+import "toastr/build/toastr.min.css";
 
 const app = new App({
   target: document.body,
